01528cc8b9e5361f71d9fad11555eb2c112a00e6
Luci12 <mihalikova.lucia@yahoo.com>
2024-06-28 00:03:31 +0200

    Initial

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username in users and users[username] == password:
+        if username in Users and Users[username] == password:
             return redirect(url_for('welcome', username=username))
         else:
             error = "Invalid username or password"
@@ -44,14 +44,14 @@ def welcome(username):
     return render_template('welcome.html', username=username)
 
 
-@app.route("/search")
-def index():
-    # Získanie údajov o cestách z databázy
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM rides")
-    trips = cur.fetchall()
-    cur.close()
-    return render_template("search.html", rides=rides)
+# @app.route("/search")
+# def search():
+#     # Získanie údajov o cestách z databázy
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM rides")
+#     trips = cur.fetchall()
+#     cur.close()
+#     return render_template("search.html", rides=rides)
 
 # Ďalšie routy a funkcie (registrácia, prihlásenie, vytváranie ciest, atď.)
 
@@ -62,37 +62,64 @@ def index():
 #         password = request.form["password"]
 #         # Implementujte pridanie používateľa do databázy
 #         # Napríklad:
-#         # cur = conn.cursor()
-#         # cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
-#         # conn.commit()
-#         # cur.close()
+#         cur = conn.cursor()
+#         cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+#         conn.commit()
+#         cur.close()
 #         return redirect(url_for("login"))  # Presmerovanie na prihlásenie
 #     return render_template("register.html")
 
 
-@app.route("/create_ad", methods=["POST"])
-def add_trip():
+@app.route("/create_ad", methods=['GET', "POST"])
+def add_rides():
     if request.method == "POST":
-        route = request.form["route"]
-        time = request.form["time"]
-        conditions = request.form["conditions"]
-        # Pridajte údaje do databázy
+        from_location = request.form["from_location"]
+        to_location = request.form["to_location"]
+        date = request.form["date"]
+        notes = request.form["notes"]
         cur = conn.cursor()
-        cur.execute("INSERT INTO trips (route, time, conditions) VALUES (%s, %s, %s)", (route, time, conditions))
+        cur.execute("INSERT INTO rides (from_location, to_location, date, notes) VALUES (%s, %s, %s)",
+                    (from_location, to_location, date, notes))
         conn.commit()
         cur.close()
-        return redirect(url_for("create_ad"))
+        return redirect(url_for('success'))
+    return redirect(url_for("create_ad"))
 
 
-@app.route("/")
-def index():
-    # Získanie údajov o cestách z databázy na základe vyhľadávania
-    search_term = "your_search_term"  # Nahraďte hľadaným výrazom
+@app.route('/success')
+def success():
+    return 'Inzerát bol úspešne vytvorený!'
+
+
+# @app.route("/search")
+# def search():
+#     # Získanie údajov o cestách z databázy na základe vyhľadávania
+#     if request.method == 'POST':
+#      from_location = request.form.get('from_location')
+#      to_location = request.form.get('to_location')
+#      filtered_rides = [ride for ride in rides if
+#                       from_location.lower() in ride['from_location'].lower() and
+#                       to_location.lower() in ride['to_location'].lower()]
+#      cur = conn.cursor()
+#      cur.execute("SELECT * FROM trips WHERE route ILIKE %s", (f"%{search_term}%",))
+#      trips = cur.fetchall()
+#      cur.close()
+#      return render_template('search.html', rides=filtered_rides)
+    # return render_template('search.html', rides=rides)
+
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    route = request.args.get("rides")
+    # Implementujte logiku pre vyhľadávanie ciest v databáze na základe zadaných kritérií
+    # Napríklad: SELECT * FROM trips WHERE route ILIKE %s;
+    # ILIKE umožňuje nezávislé na veľkosti písmen vyhľadávanie
     cur = conn.cursor()
-    cur.execute("SELECT * FROM trips WHERE route ILIKE %s", (f"%{search_term}%",))
-    trips = cur.fetchall()
+    cur.execute("SELECT * FROM rides WHERE from_location ILIKE %s", (f"%{route}%",))
+    rides = cur.fetchall()
     cur.close()
-    return render_template("search.html", trips=trips)
+    return render_template("search.html", rides=rides)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
